f5bb617b1722f2603915e28ad2dc89def3869019
Anna Glander <anna.glander@gmail.com>
2015-06-09 00:26:50 -0400

    python homework chipotle

---
+++ submissions/03_python_homework_chipotle.py
@@ -0,0 +1,74 @@
+'''
+Python Homework with Chipotle data
+https://github.com/TheUpshot/chipotle
+'''
+
+'''
+BASIC LEVEL
+PART 1: Read in the data with csv.reader() and store it in a list of lists called 'data'.
+Hint: This is a TSV file, and csv.reader() needs to be told how to handle it.
+      https://docs.python.org/2/library/csv.html
+'''
+import csv
+with open('chipotle.tsv','rU') as f:
+    data = [row for row in csv.reader(f, delimiter='\t')]
+
+
+'''
+BASIC LEVEL
+PART 2: Separate the header and data into two different lists.
+'''
+header = data[0]
+data_body = data[1:]
+
+'''
+INTERMEDIATE LEVEL
+PART 3: Calculate the average price of an order.
+Hint: Examine the data to see if the 'quantity' column is relevant to this calculation.
+Hint: Think carefully about the simplest way to do this!
+'''
+from decimal import Decimal
+#IF Item Price is the price for each item
+#price = [int(row[1]) * Decimal(row[4].strip('$')) for row in data_body]
+#average = sum(price)/int(data_body[-1][0])
+
+#IF Item Price is already equal to price*quantity
+price_total = [Decimal(row[4].strip('$')) for row in data_body]
+average_total = sum(price_total)/int(data_body[-1][0])
+ 
+'''
+INTERMEDIATE LEVEL
+PART 4: Create a list (or set) of all unique sodas and soft drinks that they sell.
+Note: Just look for 'Canned Soda' and 'Canned Soft Drink', and ignore other drinks like 'Izze'.
+'''
+sodas = [row[3] if "Canned" in row[2] else "" for row in data]
+unique_sodas = set(sodas)
+
+
+'''
+ADVANCED LEVEL
+PART 5: Calculate the average number of toppings per burrito.
+Note: Let's ignore the 'quantity' column to simplify this task.
+Hint: Think carefully about the easiest way to count the number of toppings!
+'''
+# this seems kindof hack but
+burrito_toppings = [row[3].count(',') if "Burrito" in row[2] else "" for row in data]
+burrito_toppings_only = filter(None, burrito_toppings)
+average_toppings = sum(burrito_toppings_only)/len(burrito_toppings_only)
+
+
+
+'''
+ADVANCED LEVEL
+PART 6: Create a dictionary in which the keys represent chip orders and
+  the values represent the total number of orders.
+Expected output: {'Chips and Roasted Chili-Corn Salsa': 18, ... }
+Note: Please take the 'quantity' column into account!
+Optional: Learn how to use 'defaultdict' to simplify your code.
+'''
+
+
+
+'''
+BONUS: Think of a question about this data that interests you, and then answer it!
+'''
